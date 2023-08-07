d55120c8e5f324500a257efc75cd00311ce47f76
Lee Jeongyeon <ljy5786@gmail.com>
2023-08-07 20:12:09 +0900

    Add vision API, text to speech API

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,11 @@ import 'package:flutter/material.dart';
 import 'dart:io';
 import 'package:flutter/services.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:googleapis/vision/v1.dart' as vision;
+import 'package:googleapis_auth/googleapis_auth.dart' as auth;
+import 'dart:convert';
+import 'package:googleapis/texttospeech/v1.dart' as tts; // Text-to-Speech API
+import 'package:flutter_tts/flutter_tts.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -40,6 +45,7 @@ class CameraScreenState extends State<CameraScreen> {
   late CameraController _controller; //카메라 제어를 위한 컨트롤러 선언
   late Future<void> _initializeControllerFuture; //카메라 초기화
   File? _imageFile; // 이미지 파일 변수 추가
+  late FlutterTts flutterTts; // TTS 라이브러리 인스턴스
 
 
   @override
@@ -53,6 +59,8 @@ class CameraScreenState extends State<CameraScreen> {
 
     // 이미지 파일 불러오기
     _loadImage();
+
+    flutterTts = FlutterTts(); // TTS 라이브러리 초기화
   }
 
   // 이미지 파일 불러오기 함수
@@ -71,7 +79,86 @@ class CameraScreenState extends State<CameraScreen> {
   void dispose() {
     //사용 끝나거나 강제 종료 당했을때 해제 안 하면 오류 생긴다고 해서 추가
     _controller.dispose();
+    flutterTts.stop(); // TTS 정리
     super.dispose();
+  }
+
+  Future<void> _takePicture() async {
+    if (!_controller.value.isInitialized) {
+      return;
+    }
+    try {
+      final XFile picture = await _controller.takePicture();
+      await _processImage(picture); // 사진 처리 및 Vision API 호출
+    } catch (e) {
+      print("Error taking picture: $e");
+    }
+  }
+
+  Future<void> _processImage(XFile picture) async {
+    final apiKey = Platform.environment['SPEAK_CLOTHES_VISION_API'];
+
+    if (apiKey == null) {
+      print('API key not found in environment variables.');
+      return;
+    }
+
+    final client = await auth.clientViaApiKey('SPEAK_CLOTHES_VISION_API');
+    final visionApi = vision.VisionApi(client);
+
+    final imageBytes = await picture.readAsBytes();
+    final imageContent = base64Encode(imageBytes);
+
+    final request = vision.BatchAnnotateImagesRequest.fromJson({
+      'requests': [
+        {
+          'image': {'content': imageContent},
+          'features': [{'type': 'LABEL_DETECTION'}],
+        },
+      ],
+    });
+
+    final response = await visionApi.images.annotate(request);
+    if (response.responses != null && response.responses!.isNotEmpty) {
+      final labelAnnotations = response.responses!.first.labelAnnotations;
+      if (labelAnnotations != null && labelAnnotations.isNotEmpty) {
+        final label = labelAnnotations.first.description;
+        print('Detected label: $label');
+        await _speakText('Detected label: $label'); // _speakText 함수 호출
+
+        // Text-to-Speech로 음성 재생
+        await flutterTts.setLanguage('en-US');
+        await flutterTts.setSpeechRate(0.8);
+        await flutterTts.setVolume(1.0);
+        await flutterTts.speak('Detected label: $label');
+      }
+    }
+  }
+
+  Future<void> _speakText(String text) async {
+    final apiKey = Platform.environment['SPEAK_CLOTHES_TTS_API'];
+
+    if (apiKey == null) {
+      print('Text-to-Speech API key not found in environment variables.');
+      return;
+    }
+
+    final client = await auth.clientViaApiKey(apiKey);
+    final ttsApi = tts.TexttospeechApi(client);
+
+    final synthesisInput = tts.SynthesisInput(text: text);
+    final voiceSelection = tts.VoiceSelectionParams(languageCode: 'en-US', ssmlGender: 'FEMALE');
+    final audioConfig = tts.AudioConfig(audioEncoding: 'MP3');
+
+    final ttsRequest = tts.SynthesizeSpeechRequest(
+      input: synthesisInput,
+      voice: voiceSelection,
+      audioConfig: audioConfig,
+    );
+
+    final ttsResponse = await ttsApi.text.synthesize(ttsRequest);
+
+    // TODO: ttsResponse에서 음성 파일을 재생하거나 저장할 수 있습니다.
   }
 
   @override
@@ -98,8 +185,10 @@ class CameraScreenState extends State<CameraScreen> {
         },
       ),
       // ...
+      floatingActionButton: FloatingActionButton(
+        onPressed: _takePicture, // 이미지 촬영 버튼 클릭 시 _takePicture 함수 호출
+        child: Icon(Icons.camera),
+      ),
     );
   }
-
-// ...
 }
