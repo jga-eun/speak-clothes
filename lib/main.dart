ba34b22bfa1ee61dbad8a0f90c528034ff9b18e2
leedayoung <leedayoung0601@gmail.com>
2023-08-09 17:50:34 +0900

    Committing changes before resolving divergence

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,37 @@
-import 'dart:async'; //비동기 사용하려고 import
-import 'package:camera/camera.dart'; //카메라 package 사용하려고 import
+import 'dart:async';
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'dart:io';
 import 'package:flutter/services.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:googleapis/vision/v1.dart' as vision;
+import 'package:googleapis_auth/googleapis_auth.dart' as auth;
+import 'dart:convert';
+import 'package:googleapis/texttospeech/v1.dart' as tts;
+import 'package:flutter_tts/flutter_tts.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // .env 파일에서 API 키 로드
+  await dotenv.load(fileName: ".env");
+  final visionApiKey = dotenv.env['SPEAK_CLOTHES_API'];
+  final ttsApiKey = dotenv.env['SPEAK_CLOTHES_API'];
+
   final cameras = await availableCameras();
   final firstCamera = cameras.first;
   runApp(
     MaterialApp(
       theme: ThemeData(
-        //테마는 기본값
         appBarTheme: const AppBarTheme(
-          color: Color.fromARGB(255, 230, 211, 34), // AppBar의 배경색 설정
+          color: Color.fromARGB(255, 230, 211, 34),
         ),
       ),
       home: CameraScreen(
-        camera: firstCamera, //카메라 여러개 있는 핸드폰 때문에 First라고 지정
+        camera: firstCamera,
+        visionApiKey: visionApiKey,
+        ttsApiKey: ttsApiKey,
       ),
     ),
   );
@@ -27,37 +40,41 @@ Future<void> main() async {
 class CameraScreen extends StatefulWidget {
   const CameraScreen({
     super.key,
-    required this.camera, //필수로 받아와야 하는 카메라 정보 저장 변수
+    required this.camera,
+    required this.visionApiKey,
+    required this.ttsApiKey,
   });
 
-  final CameraDescription camera; //cameradescription타입 변수 camera 선언
+  final CameraDescription camera;
+  final String? visionApiKey;
+  final String? ttsApiKey;
 
   @override
   CameraScreenState createState() => CameraScreenState();
 }
 
 class CameraScreenState extends State<CameraScreen> {
-  late CameraController _controller; //카메라 제어를 위한 컨트롤러 선언
-  late Future<void> _initializeControllerFuture; //카메라 초기화
-  File? _imageFile; // 이미지 파일 변수 추가
-
+  late CameraController _controller;
+  late Future<void> _initializeControllerFuture;
+  File? _imageFile;
+  late FlutterTts flutterTts;
+  String _analysisResult = '';
 
   @override
   void initState() {
     super.initState();
     _controller = CameraController(
-      widget.camera, //위젯에서 전달받은 카메라 정보로 컨트롤러 생성
-      ResolutionPreset.medium, //화질 높아지면 로딩 오래 걸릴 수 있다길래 우선 medium으로 설정
+      widget.camera,
+      ResolutionPreset.medium,
     );
     _initializeControllerFuture = _controller.initialize();
-
-    // 이미지 파일 불러오기
     _loadImage();
+    flutterTts = FlutterTts();
   }
 
-  // 이미지 파일 불러오기 함수
   void _loadImage() async {
-    final imageFile = await rootBundle.load('assets/speak_clothes_top_icon.png');
+    final imageFile =
+        await rootBundle.load('assets/speak_clothes_top_icon.png');
     final tempDir = await getTemporaryDirectory();
     final tempPath = '${tempDir.path}/speak_clothes_top_icon.png';
     final bytes = imageFile.buffer.asUint8List();
@@ -69,9 +86,124 @@ class CameraScreenState extends State<CameraScreen> {
 
   @override
   void dispose() {
-    //사용 끝나거나 강제 종료 당했을때 해제 안 하면 오류 생긴다고 해서 추가
     _controller.dispose();
+    flutterTts.stop();
     super.dispose();
+  }
+
+  Future<void> _takePicture() async {
+    if (!_controller.value.isInitialized) {
+      return;
+    }
+    try {
+      final XFile picture = await _controller.takePicture();
+      await _processImage(picture);
+    } catch (e) {
+      print("Error taking picture: $e");
+    }
+  }
+
+  Future<void> _processImage(XFile picture) async {
+    final apiKey = widget.visionApiKey;
+
+    if (apiKey == null) {
+      print('API key not found in environment variables.');
+      return;
+    }
+
+    final client = auth.clientViaApiKey(apiKey);
+    final visionApi = vision.VisionApi(client);
+
+    final imageBytes = await picture.readAsBytes();
+    final imageContent = base64Encode(imageBytes);
+
+    final request = vision.BatchAnnotateImagesRequest.fromJson({
+      'requests': [
+        {
+          'image': {'content': imageContent},
+          'features': [
+            {'type': 'LABEL_DETECTION'}
+          ],
+        },
+      ],
+    });
+
+    try {
+      final response = await visionApi.images.annotate(request);
+      if (response.responses != null && response.responses!.isNotEmpty) {
+        final labelAnnotations = response.responses!.first.labelAnnotations;
+        if (labelAnnotations != null && labelAnnotations.isNotEmpty) {
+          final label = labelAnnotations.first.description;
+          print('Detected label: $label');
+          await _speakText('Detected label: $label');
+
+          setState(() {
+            _analysisResult = 'Detected label: $label';
+          });
+
+          await flutterTts.setLanguage('en-US');
+          await flutterTts.setSpeechRate(0.8);
+          await flutterTts.setVolume(1.0);
+          await flutterTts.speak('Detected label: $label');
+
+          double screenWidth = MediaQuery.of(context).size.width;
+          double objectPositionX = screenWidth / 2;
+          double distanceThreshold = 20;
+          bool imageMatched = false;
+
+          while (!imageMatched) {
+            String instructionText;
+            if ((objectPositionX - screenWidth / 2).abs() < distanceThreshold) {
+              imageMatched = true;
+              await _takePicture();
+              instructionText = "촬영을 시작하겠습니다.";
+            } else if (objectPositionX > screenWidth / 2) {
+              await _speakText("왼쪽으로 이동하세요.");
+              instructionText = "왼쪽으로 이동하세요.";
+            } else {
+              await _speakText("오른쪽으로 이동하세요.");
+              instructionText = "오른쪽으로 이동하세요.";
+            }
+
+            setState(() {
+              _analysisResult = instructionText;
+            });
+          }
+        }
+      }
+    } catch (e) {
+      print("Error taking picture: $e");
+      setState(() {
+        _analysisResult = 'Error processing image';
+      });
+    }
+  }
+
+  Future<void> _speakText(String text) async {
+    final apiKey = widget.ttsApiKey;
+
+    if (apiKey == null) {
+      print('Text-to-Speech API key not found in environment variables.');
+      return;
+    }
+
+    final client = auth.clientViaApiKey(apiKey);
+    final ttsApi = tts.TexttospeechApi(client);
+
+    final synthesisInput = tts.SynthesisInput(text: text);
+    final voiceSelection =
+        tts.VoiceSelectionParams(languageCode: 'en-US', ssmlGender: 'FEMALE');
+    final audioConfig = tts.AudioConfig(audioEncoding: 'MP3');
+
+    final ttsRequest = tts.SynthesizeSpeechRequest(
+      input: synthesisInput,
+      voice: voiceSelection,
+      audioConfig: audioConfig,
+    );
+
+    final ttsResponse = await ttsApi.text.synthesize(ttsRequest);
+
+    // TODO: ttsResponse에서 음성 파일을 재생하거나 저장할 수 있음
   }
 
   @override
@@ -87,6 +219,17 @@ class CameraScreenState extends State<CameraScreen> {
                 children: [
                   CameraPreview(_controller),
                   Positioned.fill(child: Image.file(_imageFile!)),
+                  Positioned(
+                    bottom: 16,
+                    left: 16,
+                    right: 16,
+                    child: Text(
+                      _analysisResult,
+                      style: TextStyle(
+                          fontSize: 16,
+                          color: const Color.fromARGB(255, 150, 5, 5)),
+                    ),
+                  ),
                 ],
               );
             } else {
@@ -97,9 +240,10 @@ class CameraScreenState extends State<CameraScreen> {
           }
         },
       ),
-      // ...
+      floatingActionButton: FloatingActionButton(
+        onPressed: _takePicture,
+        child: Icon(Icons.camera),
+      ),
     );
   }
-
-// ...
 }
