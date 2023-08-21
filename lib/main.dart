4b7b8348b8d046aabcdefc95c43a70ea9a605ce6
Lee Jeongyeon <ljy5786@gmail.com>
2023-08-21 15:46:13 +0900

    merge loading page

--- lib/main.dart
+++ lib/main.dart
@@ -35,15 +35,110 @@ Future<void> main() async {
 
   runApp(
     MaterialApp(
-      theme: ThemeData(
-      ),
-      home: CameraScreen(
+      home:
+      LoadingMenu(
         camera: firstCamera,
         visionApiKey: visionApiKey,
-        ttsApiKey: ttsApiKey,
-      ),
+        ttsApiKey: ttsApiKey,)
     ),
   );
+}
+
+class LoadingMenu extends StatefulWidget {
+  const LoadingMenu({
+    Key? key,
+    required this.camera,
+    required this.visionApiKey,
+    required this.ttsApiKey,}) : super(key: key);
+
+  final CameraDescription camera;
+  final visionApiKey;
+  final ttsApiKey;
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _LoadingMenuState createState() {
+    return _LoadingMenuState();
+  }
+}
+
+class _LoadingMenuState extends State<LoadingMenu> {
+  double _progress = 0;
+  Timer? _timer;
+
+  @override
+  void initState() {
+    super.initState();
+    _timer = Timer.periodic(const Duration(milliseconds: 10), (timer) {
+      setState(() {
+        _progress += 0.01;
+        if (_progress >= 1) {
+          _progress = 0;
+          _timer?.cancel(); // 타이머 취소
+          Navigator.of(context).pushReplacement(MaterialPageRoute(
+            builder: (context) => CameraScreen(
+              camera: widget.camera,
+              visionApiKey: widget.visionApiKey,
+              ttsApiKey: widget.ttsApiKey,
+            ), // 카메라 화면 위젯으로 변경
+          ));
+        }
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 248, 225, 50),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              width: 230,
+              height: 230,
+              child: Stack(
+                alignment: Alignment.center,
+                children: [
+                  Transform.scale(
+                    scale:
+                    4.5, // Adjust the scale to make the CircularProgressIndicator larger
+                    child: CircularProgressIndicator(
+                      value: _progress,
+                      strokeWidth: 7,
+                      color: Colors.white,
+                      backgroundColor: const Color.fromARGB(255, 226, 208, 49),
+                    ),
+                  ),
+                  Image.network(
+                    "https://i.ibb.co/ZcbRhJd/1.jpg",
+                    width: 80,
+                    height: 80,
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.only(bottom: 80),
+              width: 110,
+              alignment: Alignment.center,
+              child: const Text(
+                'Loading',
+                style: TextStyle(fontSize: 30, color: Colors.white),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
 
 class CameraScreen extends StatefulWidget {
